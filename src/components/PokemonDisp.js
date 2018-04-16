7a55cd5bc4a64b45f338ee00a177845c638f0d9d
Bdub <bheadwhite@gmail.com>
2018-04-15 20:51:43 -0600

    redid design

--- src/components/PokemonDisp.js
+++ src/components/PokemonDisp.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react'
 import pokemonArr from './../pokemonArr'
 import './PokemonDisp.css'
 
+
 class PokemonDisp extends Component{
     constructor(){
         super()
-        this.state = {pokemonArr}
     }
     render(){
         return (
             <div className='PokemonDisp-wrapper'>
-                <ul>
 
                     {
-                        pokemonArr.map(pokemon => {
+                        pokemonArr.sort((a,b) => a.number - b.number).map(pokemon => {
+                            //trying to find out how to start a new line \n for different generation of pokemon categories.
+                            if(pokemon.name === "Mew"|| pokemon.name === "Celebi" || pokemon.name === "Spiritomb" || pokemon.name === "Genesect"){
+                            return (
+                                    <div className='PokemonDisp-container margin' >
+                                    <img src={pokemon.url} alt={pokemon.name}/>
+                                    {pokemon.name}
+                                    </div>)
+                            }
                             return (
                                 <div className='PokemonDisp-container'>
-                                <li><img src={pokemon.url} alt={pokemon.name}/>{pokemon.name}</li>
+                            <img src={pokemon.url} alt={pokemon.name}/>
+                            {pokemon.name}
                                 </div>
                             )
                         })
                     }
-                </ul>
             </div>
         )
     }
